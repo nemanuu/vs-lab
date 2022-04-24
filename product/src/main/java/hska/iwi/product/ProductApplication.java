940bc558081b26da4a7b109a82aa1b410512aec5
Bernardo <bernardo.abreufig@outlook.com>
2022-04-24 18:01:43 +0200

    dreiviertellauffähiger Zwischenstand

--- product/src/main/java/hska/iwi/product/ProductApplication.java
+++ product/src/main/java/hska/iwi/product/ProductApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories("de.hska.iwi.product")
+@EnableJpaRepositories("hska.iwi.product")
 public class ProductApplication {
 
 	public static void main(String[] args) {
