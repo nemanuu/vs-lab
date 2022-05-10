da17dcbbcff2f7d2df1a8776185a95c7a29ccc89
nemanuu <>
2022-05-10 12:52:52 +0200

    create multiple db users, add devcontainer

--- database/sql/01-create-databases.sql
+++ database/sql/01-create-databases.sql
@@ -4,4 +4,10 @@ CREATE database IF NOT EXISTS `webshop`;
 
 CREATE USER IF NOT EXISTS 'webshopuser'@'%' IDENTIFIED BY '240b2c6d58ff2ce2f508b49f';
 GRANT ALL PRIVILEGES ON *.* TO 'webshopuser'@'%';
+
+CREATE USER IF NOT EXISTS 'productuser'@'%' IDENTIFIED BY '240b2c6d58ff2ce2f508b49f';
+GRANT ALL PRIVILEGES ON *.* TO 'productuser'@'%';
+
+CREATE USER IF NOT EXISTS 'categoryuser'@'%' IDENTIFIED BY '240b2c6d58ff2ce2f508b49f';
+GRANT ALL PRIVILEGES ON *.* TO 'categoryuser'@'%';
 FLUSH PRIVILEGES;
